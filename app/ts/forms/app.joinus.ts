00a48bc825410cade64a45305910416ad10da3d5
Pankaj Rana <pankaj.rana1@timesgroup.com>
2017-12-14 16:19:00 +0530

    Deleted Msg

--- app/ts/forms/app.joinus.ts
+++
@@ -1,8 +0,0 @@
-import {Component} from 'angular2/core';
-
-@Component({
-    selector: 'my-app',
-    template: '<form><div class="sign-up"><input type="text" name="custName" /></div></form>'
-})
-
-export class JoinUsComponent {}
